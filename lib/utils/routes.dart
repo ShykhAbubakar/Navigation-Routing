02199e8bf221dc480fe7e25e7de09de4f9a96541
Abubakar Javed <shykhabubakar99@gmail.com>
2025-09-02 18:09:02 +0500

    routing update

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -3,8 +3,6 @@ import 'package:routing/home_screen.dart';
 import 'package:routing/screen_one.dart';
 import 'package:routing/screen_two.dart';
 import 'package:routing/utils/route_names.dart';
-import 'package:routing/utils/screen_three.dart';
-
 class Routes{
   static Route<dynamic> generateRoutes(RouteSettings settings){
     switch(settings.name){
@@ -14,12 +12,12 @@ class Routes{
         return MaterialPageRoute(builder: (context)=> const ScreenOne());
       case RouteNames.screenTwo:
         return MaterialPageRoute(builder: (context)=> const ScreenTwo());
-      case RouteNames.screenThree:
-        return MaterialPageRoute(builder: (context)=> const ScreenThree());
       default:
         return MaterialPageRoute(builder: (context){
-          return const Scaffold(
-            body: Text('No Routes Found'),
+          return const SafeArea(
+            child:  Scaffold(
+              body: Center(child: Text('No Routes Found')),
+            ),
           );
         });
     }
